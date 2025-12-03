6e14fed217923b92d8d8ec001c0118718a862252
MoodyNev <selamstar243@gmail.com>
2025-12-03 11:50:11 +0100

    new homework woo

---
+++ H07/A.java
@@ -0,0 +1,22 @@
+public class A {
+	public static int x = 0;
+	public int y = 7;
+	
+	public A () { // Signatur: A()
+		this(x);
+		x++;
+	}
+	
+	public A (int x) { // Signatur: A(I)
+		x = x + 2;
+		y = y - x;
+	}
+	
+	public A (double x) { // Signatur: A(D)
+		y += x;
+	}
+	
+	public void f(int i, A o) { } // Signatur: A.f(IA)
+	public void f(Long lo, A o) { } // Signatur: A.f(LA)
+	public void f(double d, A o) { } // Signatur: A.f(DA)
+}
---
+++ H07/C.java
@@ -0,0 +1,33 @@
+public class C {
+	public static void main() {
+		// a)
+		A a1 = new A(5); //(1)
+		IO.println (A.x);
+		IO.println (a1.y);
+		
+		A a2 = new A(); //(2)
+		IO.println (A.x);
+		IO.println (a2.y);
+		
+		B b = new B(); //(3)
+		IO.println (A.x);
+		IO.println (b.x);
+		IO.println (((A) b).y);
+		IO.println (b.y);
+		
+		A ab = new B(3); //(4)
+		IO.println (ab.y);
+		IO.println (((B) ab).y);
+		
+		// b)
+		int i = 1;
+		long l = 2;
+		double d = 3.0;
+		a1.f(i, a1); //(1)
+		a1.f(l, ab); //(2)
+		b.f(i, (B) ab); //(3)
+		b.f(d, ab); //(4)
+		ab.f(i, a1); //(5)
+		ab.f(i, b); //(6)
+	}
+}
