b5a38b306d84a1084503384b27ed62167a146d50
MoodyNev <selamstar243@gmail.com>
2026-01-19 01:38:24 +0100

    forgot my daily push yesterday, there is sadly not much progress

--- H11/aufgabe3.hs
+++ H11/aufgabe3.hs
@@ -1,3 +1,27 @@
+-- es gibt zwei Arten von Knoten
+-- Index-Knoten: speichern zwei Werte und können bel. Nachfolger haben
+-- Daten-Knoten: speichern nur einen Wert, keine Nachfolger
+
+-- AUFGABE A
 -- MultTree a der zur Darstellung Baum
-MultTree :: Int -> List a 
-MultTree = 
+data MultTree a = Data a
+                | Index1 a (MultTree a)
+                | Index2 a [MultTree a]
+                | Index3 a (MultTree a) (MultTree a) (MultTree a)
+                deriving Show
+
+t1 :: MultTree Int
+t1 = Index2 4
+        (Index3 3
+            (
+                Data 3
+                Data 11
+                Data 12
+            )
+        )
+        (Index2 19
+            (
+                Data 42
+                Data 23
+            )
+        )
