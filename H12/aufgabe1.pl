ea3c75bccdce6e398134dd1f8f2d1da08023508e
MoodyNev <selamstar243@gmail.com>
2026-01-27 23:19:40 +0100

    prolog homework

--- H12/aufgabe1.pl
+++ H12/aufgabe1.pl
@@ -1,11 +1,17 @@
-person(X)
-hatRang(professor, giesl)
-hatRang(assistent, rosarius)
-hatRang(assistent, kassing)
-hatRang(assistent, lommen)
-hatRang(assistent, meyer)
-hatRang(hiwi, kroebel)
-hatRang(hiwi, wittenhorst)
-hatRang(student, enial)
-hatRang(student, mart)
-hatRang(student, oll)
+hatRang(jgi, professor).
+hatRang(mlr, assistent).
+hatRang(jck, assistent).
+hatRang(nlo, assistent).
+hatRang(eme, assistent).
+hatRang(jkr, hiwi).
+hatRang(jwi, hiwi).
+hatRang(gen, student).
+hatRang(sma, student).
+hatRang(tol, student).
+bossVon(X,Y) :- hatRang(X, professor), hatRang(Y, assistent).
+bossVon(X,Y) :- hatRang(X, assistent), hatRang(Y, hiwi).
+bossVon(X,Y) :- hatRang(X, hiwi), hatRang(Y, student).
+
+hatGleichenRang(X,Y) :- bossVon(X, A), bossVon(Y,A).
+
+vorgesetzt
